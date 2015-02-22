df0cb81275c6939b4878fbbe12a69226b4683ac0
EugeneGolden <yeugenalex@gmail.com>
2015-02-22 21:34:42 +0200

    adding value by default for init

--- untitled/46 exercises/classes.py
+++ untitled/46 exercises/classes.py
@@ -2,7 +2,7 @@ __author__ = 'Admin'
 
 class FileProcessing:
     'This class returns all lines from the file'
-    def __init__(self, filename):
+    def __init__(self, filename="pol.txt"):
         self.filename = filename
 
     def openFile(self):
