d26e6db60058c106bbef1c3d35dc60d5de4565ea
EugeneGolden <yeugenalex@gmail.com>
2015-02-25 21:57:07 +0200

    changing

--- untitled/46 exercises/classes.py
+++ untitled/46 exercises/classes.py
@@ -1,19 +1,5 @@
 __author__ = 'Admin'
 
-
-class FileProcessor:
-    """This class returns all lines from the file"""
-
-    def __init__(self, filename="pol.txt"):
-        self.filename = filename
-
-    def processFile(self):
-        f = open(self.filename)
-        for line in f:
-            line = line.lower()
-            #really don't know how to put ITextReceiver.receiver here
-        f.close()
-
 class ITextReceiver:
     """This class accepts text"""
 
@@ -23,16 +9,26 @@ class ITextReceiver:
     def receiver(self, text):
         pass
 
+class FileProcessor(ITextReceiver):
+    """This class returns all lines from the file"""
 
-class WordCount:
+    def __init__(self, filename="pol.txt"):
+        self.filename = filename
+
+    def processFile(self):
+        f = open(self.filename)
+        for line in f:
+            line = line.lower()
+            ITextReceiver.receiver(line)
+            #really don't know how to put ITextReceiver.receiver here
+        f.close()
+
+class WordCount(ITextReceiver):
     """This class for counting words"""
 
-    def __init__(self, text=None):
-        self.text = text
-
-    def char_freq(self):
+    def char_freq(self, line):
         my_dict = dict()
-        for key in self.text:
+        for key in ITextReceiver.receiver(line):
             if key in my_dict:
                 my_dict[key] = my_dict[key] + 1
             else:
