b3401ef1ed2e074731b72b74fe82057b33f3076d
EugeneGolden <yeugenalex@gmail.com>
2015-02-28 22:05:57 +0200

    working version

--- untitled/46 exercises/classes.py
+++ untitled/46 exercises/classes.py
@@ -1,34 +1,30 @@
-__author__ = 'Admin'
-
 class FileProcessor:
     """This class returns line by line from the file"""
 
     def __init__(self, filename="pol.txt"):
         self.filename = filename
 
-    def processFile(self):
+    def processFile(self, calculator):
         f = open(self.filename)
         for line in f:
-            IReceiver.receiver(line)
+            calculator.receive(line)
         f.close()
 
-class IReceiver:
+class WordsCalculator:
     """This class for counting words"""
 
     my_dict = dict()
-    def receiver(self, line):
-        for key in self.line:
+    def receive(self, line):
+        for key in line:
             if key in self.my_dict:
-                 self.my_dict[key] =  self.my_dict[key] + 1
+                self.my_dict[key] = self.my_dict[key] + 1
             else:
-                 self.my_dict[key] = 1
+                self.my_dict[key] = 1
 
     def getDict(self):
         return self.my_dict
 
-
 fp = FileProcessor("pol.txt")
-ir = IReceiver
-ir.receiver(fp.processFile())
-ir.getDict()
-
+wc = WordsCalculator()
+fp.processFile(wc)
+print wc.getDict()
