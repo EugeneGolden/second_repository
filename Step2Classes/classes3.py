330564971b013ab51d4bbc966989c9894653a712
EugeneGolden <yeugenalex@gmail.com>
2015-03-07 20:33:15 +0200

    changes

--- Step2Classes/classes3.py
+++ Step2Classes/classes3.py
@@ -1,3 +1,4 @@
+__author__ = 'Admin'
 class FileProcessor:
     """This class returns line by line from the file"""
 
@@ -7,22 +8,24 @@ class FileProcessor:
     def processFile(self, calculator):
         f = open(self.filename)
         for line in f:
+            print "processFile: " + line
             calculator.receive(line)
         f.close()
 
 
 class WordsCalculator:
     """This class for counting words"""
-
     my_dict = dict()
+
     def receive(self, line):
+        print "WordsCalculator receive: " + line
         line = line.split() #split words to have elements of a list
         for key in line:
             if key in self.my_dict:
                 self.my_dict[key] = self.my_dict[key] + 1
             else:
                 self.my_dict[key] = 1
-    #It seems I need to put this method into a separate class
+            #It seems I need to put this method into a separate class
 
     def getDict(self):
         return self.my_dict
@@ -33,7 +36,8 @@ class AbstractFilter:
         self.adaptor = adaptor
 
     def receive(self, text):
-        self.adaptor.receive( self.filterText( text) )
+        filteredtext = self.filterText( text )
+        self.adaptor.receive( filteredtext )
 
     def filterText(self, text ):
         return text
@@ -42,17 +46,23 @@ class AbstractFilter:
 class LowerCaseFilter(AbstractFilter):
     """This class makes all words lowercase"""
     #TODO: how to write constructor in case of inheritance
+
     def filterText(self, text ):
-        return text.lower()
+        filteredtext = text.lower()
+        print "LowerCaseFilter converted: " + text + "  into  " + filteredtext
+        return filteredtext
 
 
 class OnlyLongLinesFilter(AbstractFilter):
     """This class makes all words lowercase"""
     #TODO: how to write constructior in case of inheritance
+
     def filterText(self, text ):
         if (len(text) > 10):
+            print "OnlyLongLinesFilter ALLOWED: " + text
             return text.lower()
         else:
+            print "OnlyLongLinesFilter BLOCKED: " + text
             return ''
 
 
@@ -60,9 +70,12 @@ filename = "pol.txt"
 fp = FileProcessor(filename)
 wc = WordsCalculator()
 fp.processFile(wc)
-print fp.processFile( LowerCaseFilter( wc ) )
-fp.processFile( OnlyLongLinesFilter( wc ) )
-fp.processFile( OnlyLongLinesFilter( LowerCaseFilter(wc) ) )
-#print "This is results of words counting regarding file %s:" % filename, wc.getDict()
-
+lwc = LowerCaseFilter( wc )
+fp.processFile(lwc)
+#filterforwc = OnlyLongLinesFilter( wc )
+#fp.processFile( filterforwc )
+lwc = LowerCaseFilter( wc )
+ollf = OnlyLongLinesFilter( lwc );
+fp.processFile( ollf )
+print "This is results of words counting regarding file %s:" % filename, wc.getDict()
 
